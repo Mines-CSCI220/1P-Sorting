453e561dd25dd4252bb17b864feffa93414b1296
Keith Hellman <khellman@mcprogramming.com>
2025-09-05 13:38:29 -0600

    starter-code for 1P-Sorting

---
+++ sorter.h
@@ -0,0 +1,37 @@
+/*
+ * sorter.h
+ *
+ * Declares a template function:
+ *    
+ *   sorter() : k-way merge sort.
+ * 
+ * 
+ * You may add other functions if you wish, but this template function
+ * is the only one that need to be exposed for the testing code.
+ * 
+ * Authors: C. Painter-Wakefield & Tolga Can
+ */
+
+#ifndef _SORTER_H
+#define _SORTER_H
+
+#include <string>
+#include <cstddef>
+#include <vector>
+
+/***
+ * DO NOT put unscoped 'using namespace std;' in header files!
+ * Instead put them at the beginning of class or function definitions
+ * (as demonstrated below).
+ *
+ * For more details, see the commentary at the top of
+ *   tests/header-sans-using-namespace.h
+ * in this project repo.
+ */
+
+template <class T>
+void sorter(std::vector<T> &items, std::size_t k) {  
+	using namespace std;
+	// write your solution for k-way merge sort below	
+}
+#endif
